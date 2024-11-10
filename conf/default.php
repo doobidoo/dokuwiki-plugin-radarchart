738f3ddd24be099e0206669078053bde5ed9d5ae
Henry <henry.krupp@gmail.com>
2024-11-10 15:26:23 +0100

    Update default.php

---
+++ conf/default.php
@@ -0,0 +1,14 @@
+; conf/default.php
+; Default configuration settings
+<?php
+$conf['width']          = 400;          // Default chart width
+$conf['height']         = 400;          // Default chart height
+$conf['colorScheme']    = 'default';    // Default color scheme
+$conf['minScale']       = 0;            // Minimum scale value
+$conf['maxScale']       = 100;          // Maximum scale value
+$conf['legendPosition'] = 'top';        // Legend position
+$conf['borderWidth']    = 2;            // Line border width
+$conf['pointRadius']    = 3;            // Data point size
+$conf['fillOpacity']    = 0.2;          // Fill opacity for areas
+$conf['containerBg']    = 'default';    // Container background
+$conf['chartBg']        = 'transparent'; // Chart area background
