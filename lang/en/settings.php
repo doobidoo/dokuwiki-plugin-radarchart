5f7e953b8cc9a8db1a028d4fee32b349fde61c9f
Henry <henry.krupp@gmail.com>
2024-11-10 15:36:30 +0100

    update

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,14 +1,13 @@
-; conf/metadata.php
-; Configuration metadata
+; lang/en/settings.php
 <?php
-$meta['width']          = array('numeric', '_min' => 100, '_max' => 1200);
-$meta['height']         = array('numeric', '_min' => 100, '_max' => 1200);
-$meta['colorScheme']    = array('multichoice', '_choices' => array('default', 'pastel', 'dark', 'neon'));
-$meta['minScale']       = array('numeric', '_min' => 0, '_max' => 1000);
-$meta['maxScale']       = array('numeric', '_min' => 0, '_max' => 1000);
-$meta['legendPosition'] = array('multichoice', '_choices' => array('top', 'bottom', 'left', 'right'));
-$meta['borderWidth']    = array('numeric', '_min' => 1, '_max' => 10);
-$meta['pointRadius']    = array('numeric', '_min' => 1, '_max' => 10);
-$meta['fillOpacity']    = array('numeric', '_min' => 0, '_max' => 1, '_step' => 0.1);
-$meta['containerBg']    = array('string');
-$meta['chartBg']        = array('string');
+$lang['width']          = 'Default chart width (pixels)';
+$lang['height']         = 'Default chart height (pixels)';
+$lang['colorScheme']    = 'Default color scheme';
+$lang['minScale']       = 'Minimum scale value';
+$lang['maxScale']       = 'Maximum scale value';
+$lang['legendPosition'] = 'Legend position';
+$lang['borderWidth']    = 'Line border width';
+$lang['pointRadius']    = 'Data point size';
+$lang['fillOpacity']    = 'Fill opacity (0-1)';
+$lang['containerBg']    = 'Container background (color or "transparent")';
+$lang['chartBg']        = 'Chart background (color or "transparent")';
